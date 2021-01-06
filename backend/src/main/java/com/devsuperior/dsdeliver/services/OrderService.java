d295cd2a79ff95585264a90db7e8e90a36415ac2
douglasNumeriano <douglas_rnmeriano@hotmail.com>
2021-01-05 21:54:33 -0300

    POST /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -1,12 +1,17 @@
 package com.devsuperior.dsdeliver.services;
 
 import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.dto.ProductDTO;
 import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.entities.OrderStatus;
+import com.devsuperior.dsdeliver.entities.Product;
 import com.devsuperior.dsdeliver.repositories.OrderRepository;
+import com.devsuperior.dsdeliver.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,6 +22,9 @@ public class OrderService {
     @Autowired
     private OrderRepository repository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     //Especifica que é uma transação somente de leitura, assim fica mais rápida
     @Transactional(readOnly = true)
     public List<OrderDTO> findAll(){
@@ -24,5 +32,22 @@ public class OrderService {
 
         //Para cada objeto da lista de Produtos transformamos em objeto DTO
         return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+    }
+
+    //Inserindo um pedido
+    @Transactional
+    public OrderDTO insert(OrderDTO dto){
+        Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(),
+                Instant.now(), OrderStatus.PENDING);
+        for(ProductDTO p : dto.getProducts()){
+            //Este método getOne vai permitir o salvamento da associação dos produtos ao pedido
+            Product product = productRepository.getOne(p.getId());
+            order.getProducts().add(product);
+        }
+
+        //Salva o objeto no banco
+        order = repository.save(order);
+
+        return new OrderDTO(order);
     }
 }
