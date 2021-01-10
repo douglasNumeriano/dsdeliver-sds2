55dfd23afd51c836465da1d2781cc086313ba3f5
douglasNumeriano <douglas_rnmeriano@hotmail.com>
2021-01-10 13:39:45 -0300

    Add Home Page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 // import Counter from "./Counter";
 
 // O Componente App está recebendo o componente Hello
 function App() {
   return (
     <div>
-      <Navbar />
+      <>
+        <Navbar />
+        <Home />
+      </>
     </div>
   );
 }
