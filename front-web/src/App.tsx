ec12c6203a02b237f16bbb1e319733642b656d9a
douglasNumeriano <douglas_rnmeriano@hotmail.com>
2021-01-10 15:13:36 -0300

    Add routes and StepsHeader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,16 +1,13 @@
 import './App.css';
-import Navbar from './Navbar';
-import Home from './Home';
+import Routes from './Routes';
+
 // import Counter from "./Counter";
 
 // O Componente App está recebendo o componente Hello
 function App() {
   return (
     <div>
-      <>
-        <Navbar />
-        <Home />
-      </>
+        <Routes />    
     </div>
   );
 }
--- front-web/src/Navbar/index.tsx
+++ front-web/src/Navbar/index.tsx
@@ -1,12 +1,14 @@
 import './styles.css'
 // importante um svg
 import {ReactComponent as Logo} from './logo.svg'
+import { Link } from 'react-router-dom';
+
 function Navbar(){
     return(
         // className no react é a mesma coisa que class em HTML
         <nav className="main-navbar">
             <Logo />
-            <a href="home" className="logo-test">DS Delivery</a>
+            <Link to="/" className="logo-test">DS Delivery</Link>
         </nav>           
     )
 }
